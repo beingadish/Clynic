13005c42dbe4d844de2c4622728b57aa88c4dadc
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2025-03-19 19:08:25 +0530

    Basic Project Setup

---
+++ Clynic-Service/src/main/java/com/beingadish/projects/clynicservice/ClynicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beingadish.projects.clynicservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClynicServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClynicServiceApplication.class, args);
+    }
+
+}
---
+++ Clynic-Service/src/test/java/com/beingadish/projects/clynicservice/ClynicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beingadish.projects.clynicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClynicServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
