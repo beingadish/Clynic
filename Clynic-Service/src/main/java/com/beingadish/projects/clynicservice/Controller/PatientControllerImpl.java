8b65e2ebc17fd11725ec7b6a5d18fbac5fa9c96c
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2025-03-24 15:18:00 +0530

    feat: Added the Dockerfile & Deployed the PostgreSQL DB in Docker & also ran the Dockerfile of Clynic Service Application

--- Clynic-Service/src/main/java/com/beingadish/projects/clynicservice/Controller/PatientControllerImpl.java
+++ Clynic-Service/src/main/java/com/beingadish/projects/clynicservice/Controller/PatientControllerImpl.java
@@ -4,7 +4,8 @@ import com.beingadish.projects.clynicservice.DTO.PatientRequestDTO;
 import com.beingadish.projects.clynicservice.DTO.PatientResponseDTO;
 import com.beingadish.projects.clynicservice.DTO.Validators.CreatePatientValidationGroup;
 import com.beingadish.projects.clynicservice.Service.PatientService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing patients") // Swagger UI Implementation
 public class PatientControllerImpl implements PatientController {
 
     private final PatientService patientService;
@@ -25,6 +27,7 @@ public class PatientControllerImpl implements PatientController {
 
     @Override
     @GetMapping
+    @Operation(summary = "Get Patients")
     public ResponseEntity<List<PatientResponseDTO>> getPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
@@ -32,18 +35,21 @@ public class PatientControllerImpl implements PatientController {
 
     @Override
     @PostMapping
+    @Operation(summary = "Create Patient")
     public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patient) {
         return ResponseEntity.ok().body(patientService.createPatient(patient));
     }
 
     @Override
     @PutMapping("/{id}")
+    @Operation(summary = "Update Patient")
     public ResponseEntity<PatientResponseDTO> updatePatient(@Validated({Default.class}) @RequestBody PatientRequestDTO patient, @PathVariable UUID id) {
         return ResponseEntity.ok().body(patientService.updatePatient(id, patient));
     }
 
     @Override
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Patient")
     public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
         patientService.deletePatient(id);
         return ResponseEntity.noContent().build();
