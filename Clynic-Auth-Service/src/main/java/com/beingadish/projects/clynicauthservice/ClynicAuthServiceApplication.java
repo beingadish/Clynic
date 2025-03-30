642e8dec2d7e087011730a7e54fbd7f859f43c0a
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2025-03-30 19:18:39 +0530

    feat(clynic-auth-service): Added new Auth-Service Microservice Module
    This module will be responsible for all the Auth Related Queries & JWT Token Generation, Authentication & Authorisation services of the whole architecture

---
+++ Clynic-Auth-Service/src/main/java/com/beingadish/projects/clynicauthservice/ClynicAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beingadish.projects.clynicauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClynicAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClynicAuthServiceApplication.class, args);
+	}
+
+}
---
+++ Clynic-Auth-Service/src/test/java/com/beingadish/projects/clynicauthservice/ClynicAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beingadish.projects.clynicauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClynicAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
