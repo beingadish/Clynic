3fed31de33abd6cc4416cfa706070648a3a9d831
Aadarsh Pandey <aadarshpandey.pandey@gmail.com>
2025-03-30 21:30:53 +0530

    feat(Auth-Service): JWT Token Generation & Password Encoder along with Security Configuration Done

--- Clynic-Auth-Service/src/main/java/com/beingadish/projects/clynicauthservice/Service/AuthServiceImpl.java
+++ Clynic-Auth-Service/src/main/java/com/beingadish/projects/clynicauthservice/Service/AuthServiceImpl.java
@@ -2,6 +2,8 @@ package com.beingadish.projects.clynicauthservice.Service;
 
 import com.beingadish.projects.clynicauthservice.DTO.LoginRequestDTO;
 import com.beingadish.projects.clynicauthservice.Model.User;
+import com.beingadish.projects.clynicauthservice.Utils.JWTUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,18 +13,23 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserService userService;
 
-    public AuthServiceImpl(UserService userService) {
+    private final PasswordEncoder passwordEncoder;
+
+    private final JWTUtil jwtUtil;
+
+    public AuthServiceImpl(UserService userService, PasswordEncoder passwordEncoder, JWTUtil jwtUtil) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
 
-        // Find the User -> Match the Password -> Generate the JWT using Role & Email
-        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+        // Find the User -> Match the Password -> Generate the JWT using Role & Email --> Return
+
+        return userService.findByEmail(loginRequestDTO.getEmail())
                 .filter(u -> passwordEncoder.matches(u.getPassword(),loginRequestDTO.getPassword()))
                 .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
-
-        return token;
     }
 }
